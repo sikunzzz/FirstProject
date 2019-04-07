04d4d8b0a766fd1953456202977a34233113f3ac
sikunzzz <sikun.zeng95@gmail.com>
2019-04-07 18:20:28 +0100

    debugged and completion of classes and functions

--- Python_Course_Project/class_def.py
+++ Python_Course_Project/class_def.py
@@ -1,104 +1,181 @@
-
-import pandas as pd
-from scipy.stats import norm
-import numpy as np
+from functions_def import *
 import matplotlib.pyplot as plt
 
-from functions_def import intra_day_volatility
 
+# Use factory pattern design to analyse hedging pnl of option
 
-class OptionsHedgingPnl:
+class OptionsHedgingData:
+    """ Base class to for selection data """
 
-    def __init__(self, dta, r, q):
-        self.dta = dta
+    def __init__(self, r, q):
+        """ Initialise member of foreign and base currency interest rate"""
         self.r = r
         self.q = q
 
-    def option_price(self):
-        d1 = (np.log(self.s[:-1]/self.k) + (self.r - self.q + self.sigma[:-1]**2/2)*self.tau[:-1])/(self.sigma[:-1]*self.tau[:-1])
-        d2 = d1 - self.sigma[:-1]*np.sqrt(self.tau[:-1])
-        return self.s[:-1]*np.exp(-self.q*self.tau[:-1])*norm.cdf(d1) - self.k * np.exp(-self.r*self.tau[:-1])*norm.cdf(d2)
 
-    def delta(self):
-        d1 = (np.log(self.s[:-1]/self.k) + (self.r - self.q + self.sigma[:-1]**2/2)*self.tau[:-1])/(self.sigma[:-1]*self.tau[:-1])
-        return np.exp(-self.q*self.tau[:-1]) * norm.cdf(d1)
-
-    def vega(self):
-        d1 = (np.log(self.s[:-1]/self.k) + (self.r - self.q + self.sigma[:-1]**2/2)*self.tau[:-1])/(self.sigma[:-1]*self.tau[:-1])
-        return self.s[:-1]*np.exp(-self.q*self.tau[:-1])*norm.pdf(d1)*np.sqrt(self.tau[:-1])
-
-    def duration_data_selector(self):
-        if self.duration == "1M":
-            new_dta = self.dta[self.dta["1M Maturity"]>=0]
-            return new_dta, new_dta.index, new_dta["Close"].values, new_dta["1M Maturity"].values, new_dta["V1M"].values
-        elif self.duration == "3M":
-            new_dta = self.dta[self.dta["3M Maturity"]>=0]
-            return new_dta, new_dta.index, new_dta["Close"].values, new_dta["3M Maturity"].values, new_dta["V3M"].values
-        elif self.duration == "1W":
-            new_dta = self.dta[self.dta["1W Maturity"]>=0]
-            return new_dta, new_dta.index, new_dta["Close"].values, new_dta["1W Maturity"].values, new_dta["V1W"].values
-
-    def hedging_pnl(self):
-        optionPV = self.option_price()
-        repl_port = np.zeros((len(self.tau), 1))
-        total_port = np.zeros((len(self.tau), 1))
-        bankacc = np.zeros((len(self.tau), 1))
-        delta_vec = self.delta()
-
-        stock_pos = delta_vec[1:] * self.s[1:-1]
-        stock_pos_diff = stock_pos - delta_vec[:-1] * self.s[1:-1]
-        bankacc[0] = optionPV[0] - delta_vec[0]*self.s[0]
-        repl_port[0] = optionPV[0]
-        dt = np.diff(self.tau)
-
-        for t in range(len(dt)-1):
-            bankacc[t+1] = bankacc[t]*(1 + dt[t]*self.r) - stock_pos_diff[t] *(1-dt[t]*self.q)
-            repl_port[t+1] = stock_pos[t] + bankacc[t+1]
-            total_port[t+1] = optionPV[t+1] - repl_port[t+1] - bankacc[0]*(1 + self.r*dt[0])**self.tau[t+1]
-
-        optionPV =np.append(optionPV, np.max(self.s[-1] - self.k, 0))
-        bankacc[-1] = bankacc[-2]*(1+dt[-1]*self.r)
-        repl_port[-1] = delta_vec[-1]*self.s[-1] + bankacc[-1]
-        total_port[-1] = optionPV[-1] - repl_port[-1] - bankacc[0]* (1+ self.r*dt[0])**self.tau[-1]
-
-        return optionPV, repl_port, total_port
-
-
-class OptionHedgingPnl1W(OptionsHedgingPnl):
+class OptionHedgingData1W(OptionsHedgingData):
+    """ Subclass for selecting 1W data"""
 
     def __init__(self, dta, r, q):
-        super().__init__(dta, r, q)
+        """ Initialise using the base class, and further initialisatin"""
+        super().__init__(r, q)
         self.duration = "1W"
-        self.dta, self.date, self.s, self.tau, self.sigma = self.duration_data_selector()
+        self.dta = dta
+        self.date, self.s, self.tau, self.sigma = dta.index, dta["Close"].values, dta["1W Maturity"].values, dta["V1W"].values
         self.k = self.s[0]
 
 
-class OptionHedgingPnl1M(OptionsHedgingPnl):
+class OptionHedgingData1M(OptionsHedgingData):
+    """ Subclass for selecting 1M data"""
 
     def __init__(self, dta, r, q):
-        super().__init__(dta, r, q)
+        """ Initialise using the base class, and further initialisatin"""
+        super().__init__(r, q)
         self.duration = "1M"
-        self.dta, self.date, self.s, self.tau, self.sigma = self.duration_data_selector()
+        self.dta = dta
+        self.date, self.s, self.tau, self.sigma = dta.index, dta["Close"].values, dta["1M Maturity"].values, dta["V1M"].values
         self.k = self.s[0]
 
 
-class OptionHedgingPnl3M(OptionsHedgingPnl):
+class OptionHedgingData3M(OptionsHedgingData):
+    """ Subclass for selecting 3M data"""
 
     def __init__(self, dta, r, q):
-        super().__init__(dta, r, q)
+        """ Initialise using the base class, and further initialisatin """
+        super().__init__(r, q)
         self.duration = "3M"
-        self.dta, self.date, self.s, self.tau, self.sigma = self.duration_data_selector()
+        self.dta = dta
+        self.date, self.s, self.tau, self.sigma = dta.index, dta["Close"].values, dta["3M Maturity"].values, dta["V3M"].values
         self.k = self.s[0]
+
+
+class Analysis:
+    """
+    Analysis class contains functions to plot for results using different dynamic hedging methods
+    """
+
+    def hedging_pnl_plot(self, option, pv_option):
+        """
+
+        :param option: one of subclasses of OptionHedgingData
+        :param pv_option: integer, range from 0 to 2, representing
+        option price path, replicating portfolio value path, total portfolio value path respectively
+        :return: graphs for dynamic hedging pnl
+        """
+        results = hedging_pnl(option.s, option.tau, option.sigma, option.k, option.r, option.q)
+        plt.plot(option.date, results[pv_option])
+        plt.xlabel("Date")
+        plt.ylabel("Path")
+        plt.title("Value Path for plain dynamic hedging")
+        plt.show()
+
+    def hedging_pnl_band_plot(self, option, pv_option, func):
+        """
+
+        :param option: one of subclasses of OptionHedgingData
+        :param pv_option: integer, range from 0 to 2, representing
+        option price path, replicating portfolio value path, total portfolio value path respectively
+        :param func: delta/vega functions
+        :return: graphs for dynamic hedging pnl using delta/vega bound
+        """
+        if pv_option == 0:
+            return self.hedging_pnl_plot(option, pv_option)
+        else:
+            del_vec = func(option.s[:-1], option.tau[:-1], option.sigma[:-1], option.k, option.r, option.q)
+            diff_vec = np.diff(del_vec)
+            min_del, max_del = np.abs(np.min(diff_vec)), np.max(np.cumsum(diff_vec))
+            bound = np.linspace(min_del, max_del, num=1000)
+            final_pnl = [
+                hedging_pnl_delta_band(option.s, option.tau, option.sigma, option.k, option.r, option.q, x, func)[
+                    pv_option][-1] for x in bound]
+            plt.plot(bound, final_pnl)
+            plt.xlabel("Bound")
+            plt.ylabel("Pnl at Maturity")
+            plt.title("Pnl at maturity for various hedging bound")
+            plt.show()
+
+    def intraday_vol_plot(self, option):
+        """
+
+        :param option: one of subclasses of OptionHedgingData
+        :return: graph for intra day volatility
+        """
+        date_range = option.date.normalize().unique()
+        intra_vol_vec = [intra_day_volatility(option.dta, option.date, x) for x in date_range]
+        plt.plot(date_range, intra_vol_vec)
+        plt.xlabel("Date")
+        plt.ylabel("Intra-day Volatility")
+        plt.title("Intra-day Volatility for the duration of the option")
+        plt.show()
+
+
+class AnalysisFactory:
+    """ Factory of Analysis class to select which method to use"""
+
+    def get_pnl_method(self, option, pv_option, method):
+        """
+        :param option: one of subclasses of OptionHedgingData
+        :param pv_option: integer, range from 0 to 2, representing
+        :param method: 'plain', 'delta', 'vega', 'day_vol'
+        :return: function in Analysis class
+        """
+        analysis = Analysis()
+        if method == "plain":
+            return analysis.hedging_pnl_plot(option, pv_option)
+        elif method == "delta" or method == "vega":
+            return analysis.hedging_pnl_band_plot(option, pv_option, eval(method))
+        elif method == "day_vol":
+            return analysis.intraday_vol_plot(option)
+        else:
+            ValueError(method)
+
+
+class OptionFactory:
+    """ Factory of OptionHedging subclass to select which duration to use"""
+
+    def get_option(self, dta, duration, r, q):
+        """
+
+        :param dta: pandas data frame
+        :param duration: '1W'. '1M', '3M'
+        :param r: interest rate for foreign currency
+        :param q: interest rate for base currency
+        :return: subclass of OptionHedgingData
+        """
+        if duration == "1W":
+            new_dta = dta[dta["1W Maturity"] >= 0]
+            return OptionHedgingData1W(new_dta, r, q)
+        elif duration == "1M":
+            new_dta = dta[dta["1M Maturity"] >= 0]
+            return OptionHedgingData1M(new_dta, r, q)
+        elif duration == "3M":
+            new_dta = dta[dta["3M Maturity"] >= 0]
+            return OptionHedgingData3M(new_dta, r, q)
+        else:
+            ValueError(duration)
+
+
+class OptionInitializeAndAnalysis:
+    """ The final control that select from OptionFactory and AnalysisFactory"""
+
+    def initialise(self, dta, duration, r, q, pv_option, method="plain"):
+        """
+        :param dta: pandas data frame
+        :param duration: '1W'. '1M', '3M'
+        :param r: interest rate for foreign currency
+        :param q: interest rate for base currency
+        :param pv_option: integer, range from 0 to 2, representing
+        :param method: 'plain', 'delta', 'vega', 'day_vol'
+        :return: function in Analysis
+        """
+        optionfac = OptionFactory()
+        option = optionfac.get_option(dta, duration, r, q)
+        analysisfac = AnalysisFactory()
+        return analysisfac.get_pnl_method(option, pv_option, method)
 
 
 if __name__ == "__main__":
     df = pd.read_excel("USDBRL_PriceHist.xlsx", index_col=0)
-    option3m_analysis = OptionHedgingPnl3M(df, 0.065, 0.02)
-    # op_pv, repl_pv, total_pv = option1m_analysis.hedging_pnl()
-    # plt.plot(option1m_analysis.date, op_pv)
-    date_range = option3m_analysis.date.normalize().unique()
-    intra_vol_vec = [intra_day_volatility(option3m_analysis.dta, option3m_analysis.date, x) for x in date_range]
-    plt.plot(intra_vol_vec)
-    plt.show()
-
-
+    trial_analysis = OptionInitializeAndAnalysis()
+    trial_analysis.initialise(df, "1M", 0.065, 0.02, 2, "delta")
+    trial_analysis.initialise(df, "1M", 0.065, 0.02, 2, "day_vol")
--- Python_Course_Project/functions_def.py
+++ Python_Course_Project/functions_def.py
@@ -1,91 +1,173 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import datetime
 from scipy.stats import norm
 
 
 def daily_data_wrapper(func):
-    def intra_day_volatility_wrapper(dta, Dates, start_date):
-        new_dta = func(dta, Dates, start_date)
+
+    """
+    function wrapper to calculate intra day volatility
+
+    :param func: function that select specific data
+    :return: function wrapper that calculate volatility
+    """
+
+    def intra_day_volatility_wrapper(dta, alldates, start_date):
+        new_dta = func(dta, alldates, start_date)
         close_p = new_dta["Close"].values
         return np.sqrt(252*8*60)*np.std(close_p[1:]/close_p[:-1] - 1)
     return intra_day_volatility_wrapper
 
 
 @daily_data_wrapper
-def intra_day_volatility(dta, Dates, start_date):
-    return dta[np.logical_and(Dates.month == start_date.month, Dates.day == start_date.day)]
+def intra_day_volatility(dta, alldates, start_date):
+    """
+    Data selector
+
+    :param dta: pandas data frame
+    :param alldates: dates in dta
+    :param start_date: specific date to select
+    :return:
+    """
+    return dta[np.logical_and(alldates.month == start_date.month, alldates.day == start_date.day)]
 
 
 def duration_data_selector(dta, duration):
+    """
+    Choose data for specific volatility duration
+
+    :param dta: pandas data frame
+    :param duration: 1W, 1M, 3M
+    :return: date, close price, Time to maturity, volatility
+    """
 
     if duration == "1M":
-        new_dta = dta[dta["1M Maturity"]>=0]
+        new_dta = dta[dta["1M Maturity"] >= 0]
         return new_dta.index, new_dta["Close"].values, new_dta["1M Maturity"].values, new_dta["V1M"].values
     elif duration == "3M":
-        new_dta = dta[dta["3M Maturity"]>=0]
+        new_dta = dta[dta["3M Maturity"] >= 0]
         return new_dta.index, new_dta["Close"].values, new_dta["3M Maturity"].values, new_dta["V3M"].values
     elif duration == "1W":
-        new_dta = dta[dta["1W Maturity"]>=0]
+        new_dta = dta[dta["1W Maturity"] >= 0]
         return new_dta.index, new_dta["Close"].values, new_dta["1W Maturity"].values, new_dta["V1W"].values
 
 
 def option_price(s, tau, sigma, k, r, q):
+    """
+    Calculate Black-Scholes option price
+
+    :param s: stock price
+    :param tau: time to maturity
+    :param sigma: volatility
+    :param k: strike price
+    :param r: interest rate in foreign currency
+    :param q: interest rate in base currency
+    :return: option price
+    """
+
     d1 = (np.log(s/k) + (r - q + sigma**2/2)*tau)/(sigma*tau)
     d2 = d1 - sigma*np.sqrt(tau)
     return s*np.exp(-q*tau)*norm.cdf(d1) - k * np.exp(-r*tau)*norm.cdf(d2)
 
 
 def delta(s, tau, sigma, k, r, q):
+    """
+    Calculate Black-Scholes delta
+
+    :param s: stock price
+    :param tau: time to maturity
+    :param sigma: volatility
+    :param k: strike price
+    :param r: interest rate in foreign currency
+    :param q: interest rate in base currency
+    :return: delta
+    """
+
     d1 = (np.log(s/k) + (r - q + sigma**2/2)*tau)/(sigma*tau)
     return np.exp(-q*tau) * norm.cdf(d1)
 
 
 def vega(s, tau, sigma, k, r, q):
+    """
+    Calculate Black-Scholes vega
+
+    :param s: stock price
+    :param tau: time to maturity
+    :param sigma: volatility
+    :param k: strike price
+    :param r: interest rate in foreign currency
+    :param q: interest rate in base currency
+    :return: vega
+    """
+
     d1 = (np.log(s/k) + (r - q + sigma**2/2)*tau)/(sigma*tau)
     return s*np.exp(-q*tau)*norm.pdf(d1)*np.sqrt(tau)
 
 
 def hedging_pnl(s, tau, sigma, k, r, q):
+    """
+    Calculate pnl for dynamic delta hedging for every step
 
-    optionPV = option_price(s[:-1],tau[:-1], sigma[:-1],k, r,q)
+    :param s: stock price
+    :param tau: time to maturity
+    :param sigma: volatility
+    :param k: strike price
+    :param r: interest rate in foreign currency
+    :param q: interest rate in base currency
+    :return: option price path, replicating portfolio value path, total portfolio value path
+    """
+
+    option_pv = option_price(s[:-1], tau[:-1], sigma[:-1], k, r, q)
     repl_port = np.zeros((len(tau), 1))
     total_port = np.zeros((len(tau), 1))
-    bankacc = np.zeros((len(tau), 1))
+    bank_acc = np.zeros((len(tau), 1))
     delta_vec = delta(s[:-1], tau[:-1], sigma[:-1], k, r, q)
 
     stock_pos = delta_vec[1:] * s[1:-1]
     stock_pos_diff = stock_pos - delta_vec[:-1] * s[1:-1]
-    bankacc[0] = optionPV[0] - delta_vec[0]*s[0]
-    repl_port[0] = optionPV[0]
+    bank_acc[0] = option_pv[0] - delta_vec[0]*s[0]
+    repl_port[0] = option_pv[0]
     dt = np.diff(tau)
 
     for t in range(len(dt)-1):
-        bankacc[t+1] = bankacc[t]*(1 + dt[t]*r) - stock_pos_diff[t] *(1-dt[t]*q)
-        repl_port[t+1] = stock_pos[t] + bankacc[t+1]
-        total_port[t+1] = optionPV[t+1] - repl_port[t+1] - bankacc[0]*(1 + r*dt[0])**tau[t+1]
+        bank_acc[t+1] = bank_acc[t]*(1 + dt[t]*r) - stock_pos_diff[t] *(1-dt[t]*q)
+        repl_port[t+1] = stock_pos[t] + bank_acc[t+1]
+        total_port[t+1] = option_pv[t+1] - repl_port[t+1] - bank_acc[0]*(1 + r*dt[0])**tau[t+1]
 
-    optionPV = np.append(optionPV, np.max(s[-1] - k, 0))
-    bankacc[-1] = bankacc[-2]*(1+dt[-1]*r)
-    repl_port[-1] = delta_vec[-1]*s[-1] + bankacc[-1]
-    total_port[-1] = optionPV[-1] - repl_port[-1] - bankacc[0]* (1+ r*dt[0])**tau[-1]
+    option_pv = np.append(option_pv, np.maximum(s[-1] - k, 0))
+    bank_acc[-1] = bank_acc[-2]*(1+dt[-1]*r)
+    repl_port[-1] = delta_vec[-1]*s[-1] + bank_acc[-1]
+    total_port[-1] = option_pv[-1] - repl_port[-1] - bank_acc[0]*(1+r*dt[0])**tau[-1]
 
-    return optionPV, repl_port, total_port
+    return option_pv, repl_port, total_port
 
 
-def hedging_pnl_delta_band(s, tau, sigma, k, r, q, bound, func ):
-    optionPV = option_price(s[:-1],tau[:-1],sigma[:-1], k, r,q)
+def hedging_pnl_delta_band(s, tau, sigma, k, r, q, bound, func):
+    """
+    Calculate pnl for dynamic delta hedging according to a pre-set delta or vega bound
+
+    :param s: stock price
+    :param tau: time to maturity
+    :param sigma: volatility
+    :param k: strike price
+    :param r: interest rate in foreign currency
+    :param q: interest rate in base currency
+    :param bound: delta/vega bound
+    :param func: delta or vega functions as defined above
+    :return: option price path, replicating portfolio value path, total portfolio value path
+    """
+
+    option_pv = option_price(s[:-1], tau[:-1], sigma[:-1], k, r, q)
     delta_vec = func(s[:-1], tau[:-1], sigma[:-1], k, r, q)
 
     repl_port = np.array([])
     total_port = np.array([])
-    bankacc = np.array([])
+    bank_acc = np.array([])
 
-    repl_port = np.append(repl_port, optionPV[0])
+    repl_port = np.append(repl_port, option_pv[0])
     total_port = np.append(total_port, 0)
-    bankacc = np.append(bankacc, optionPV[0] - s[0]*delta_vec[0])
+    bank_acc = np.append(bank_acc, option_pv[0] - s[0]*delta_vec[0])
     delta_diff = np.diff(delta_vec)
     cumulate_delta = 0
     time_stamp = 0
@@ -96,23 +178,17 @@ def hedging_pnl_delta_band(s, tau, sigma, k, r, q, bound, func ):
         cumulate_delta += delta_diff[i]
         if np.abs(cumulate_delta) >= bound:
             stock_pos = (s[i+1] - s[stock_index]) * cumulate_delta
-            bankacc = np.append(bankacc, bankacc[-1]*(1 + dt*r)**(t - time_stamp) - stock_pos*(1-dt*q)**(t - time_stamp))
-            repl_port = np.append(repl_port, bankacc[-1] + delta_vec[i+1] * s[i+1])
-            total_port = np.append(total_port,  optionPV[i+1] - repl_port[-1] - bankacc[0] * (1 + dt*r)**t)
+            bank_acc = np.append(bank_acc, bank_acc[-1]*(1 + dt*r)**(t - time_stamp) - stock_pos*(1-dt*q)**(t - time_stamp))
+            repl_port = np.append(repl_port, bank_acc[-1] + delta_vec[i+1] * s[i+1])
+            total_port = np.append(total_port,  option_pv[i+1] - repl_port[-1] - bank_acc[0] * (1 + dt*r)**t)
             stock_index = i+1
             cumulate_delta = 0
             time_stamp = t
 
-    stock_pos = s[-1] - s[stock_index] * cumulate_delta
-    optionPV = np.append(optionPV, np.max(s[-1] - k, 0))
-    bankacc = np.append(bankacc, bankacc[-1]*(1 + dt*r)**(t - time_stamp) - stock_pos*(1-dt*q)**(t - time_stamp))
-    repl_port = np.append(repl_port, bankacc[-1] + delta_vec[-1] * s[-1])
-    total_port = np.append(total_port, optionPV[-1] - repl_port[-1] - bankacc[0] * (1 + dt*r)**tau[-1])
+    stock_pos = (s[-1] - s[stock_index]) * cumulate_delta
+    option_pv = np.append(option_pv, np.maximum(s[-1] - k, 0))
+    bank_acc = np.append(bank_acc, bank_acc[-1]*(1 + dt*r)**(tau[-1] - time_stamp) - stock_pos*(1-dt*q)**(tau[-1] - time_stamp))
+    repl_port = np.append(repl_port, bank_acc[-1] + delta_vec[-1] * s[-1])
+    total_port = np.append(total_port, option_pv[-1] - repl_port[-1] - bank_acc[0] * (1 + dt*r)**tau[-1])
 
-    return optionPV, repl_port, total_port
-
-
-if __name__ == "__main__":
-    df = pd.read_excel("USDBRL_PriceHist.xlsx", index_col= 0)
-    dates, s, tau, sigma = duration_data_selector(df, "1M")
-    opPV, repPV, totalPV = hedging_pnl_delta_band(s, tau, sigma, s[0], 0.065, 0.02, 0.000001, delta)
+    return option_pv, repl_port, total_port
